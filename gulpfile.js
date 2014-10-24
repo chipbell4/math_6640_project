1e2b3a5ac83a2c2e1930a8013b1bc34a8ec329bd
Chip Bell <chip.bell@spinen.com>
2014-10-24 15:31:33 -0400

    Stubbing out build tools

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+var gulp = require('gulp');
+var browserify = require('gulp-browserify');
+var jshint = require('gulp-jshint');
+var concat = require('gulp-concat'); 
+
+gulp.task('jshint', function() {
+	return gulp.src('js/*')
+		.pipe(jshint());
+});
+
+gulp.task('browserify', function() {
+	return gulp.src('js/main.js')
+		.pipe(browserify({}))
+		.pipe(gulp.dest('./build'));
+});
+
+
+gulp.task('default', ['jshint', 'browserify']);
+
+gulp.task('watch', function() {
+	gulp.watch('js/*', ['default']);
+});
