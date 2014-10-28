1317a685b1cee9e275e3e93395c51a9f04d8d22c
Chip Bell <chip.bell@spinen.com>
2014-10-27 22:31:46 -0400

    Adding a class for point containment checking

---
+++ js/PolygonUtils/PolygonPointContainmentChecker.js
@@ -0,0 +1,39 @@
+var PolygonPointContainmentChecker = function(points) {
+	this.points = points;
+};
+
+/**
+ * Calculates the winding number for a point only at a particular vertex of the polygon, given by the index
+ */
+PolygonPointContainmentChecker.prototype.calculateWindingForPointAtIndex = function(point, index) {
+	var currentPoint = this.points[index].clone();
+	var nextPoint = this.points[ (index + 1) % this.points.length ].clone();
+
+	// The polygon edge
+	var firstAxis = nextPoint.sub(currentPoint);
+
+	// A vector from point[i] to the provided point
+	var secondAxis = point.clone().sub(currentPoint);
+
+	// return the sign (the z-component) of the cross product to determine which side of the axis the point is
+	var crossProduct = firstAxis.cross(secondAxis);
+
+	if(crossProduct.z < 0) {
+		return -1;
+	}
+
+	return 1;
+}
+
+PolygonPointContainmentChecker.prototype.containsPoint = function(point) {
+	var winding = 0;
+
+	var N = this.points.length;
+	for(var i = 0; i < N; i++) {
+		winding += this.calculateWindingForPointAtIndex(point, i);
+	}
+
+	return winding == N;
+};
+
+module.exports = PolygonPointContainmentChecker;
---
+++ tests/PolygonUtils/PolygonPointContainmentChecker.js
@@ -0,0 +1,32 @@
+var expect = require('chai').expect;
+var THREE = require('three');
+var PolygonPointContainmentChecker = require('../../js/PolygonUtils/PolygonPointContainmentChecker.js');
+
+describe('PolygonPointContainmentChecker', function() {
+	it('Should exist', function() {
+		expect(PolygonPointContainmentChecker).to.be.ok;
+	});
+	
+	var pointSet = [
+		new THREE.Vector3(0, 0, 0),
+		new THREE.Vector3(1, 0, 0),
+		new THREE.Vector3(1, 1, 0),
+		new THREE.Vector3(0, 1, 0),
+	];
+
+	var checker = new PolygonPointContainmentChecker(pointSet);
+
+	it('Should be able to determine if a point is inside the polygon', function() {
+		var candidate = new THREE.Vector3(0.5, 0.5, 0);
+
+		expect(checker.containsPoint(candidate)).to.be.ok;
+	});
+
+	it('Should be able to determine if a point is outside the polygon', function() {
+		var candidate = new THREE.Vector3(0.5, 1.5, 0);
+
+		expect(checker.containsPoint(candidate)).to.be.not.ok;
+	});
+
+
+});
