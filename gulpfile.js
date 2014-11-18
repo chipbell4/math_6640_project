5bf563d947ded7c7b15f67b068e61a64722e9d04
Chip Bell <chip.bell@spinen.com>
2014-11-17 20:29:02 -0500

    Adding task for generating the paper

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var browserify = require('gulp-browserify');
 var jshint = require('gulp-jshint');
 var mocha = require('gulp-mocha');
+var exec = require('gulp-exec');
 
 gulp.task('jshint', function() {
 	return gulp.src('js/**/*.js')
@@ -20,6 +21,20 @@ gulp.task('browserify', ['jshint', 'test'], function() {
 		.pipe(gulp.dest('./build'));
 });
 
+gulp.task('paper', function(cb) {
+    var options = {
+        continueOnError: false,
+        pipeStdout: false
+    };
+    var reportOptions = {
+        err: true,
+        stderr: true,
+        stdout: true
+    };
+    gulp.src('paper/main.tex')
+        .pipe(exec('pdflatex -output-directory paper/ <%= file.path %>'))
+        .pipe(exec.reporter(reportOptions));
+});
 
 gulp.task('default', ['jshint', 'test', 'browserify']);
 
