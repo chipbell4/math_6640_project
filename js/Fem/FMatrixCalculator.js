e716848150ed4b70a9478b344d9ae7c115e584df
Chip Bell <chip.bell@spinen.com>
2014-11-28 13:38:16 -0500

    Adding the calculator for F

---
+++ js/Fem/FMatrixCalculator.js
@@ -0,0 +1,23 @@
+
+var FMatrixCalculator = function(femGeometry, weight) {
+    this.geometry = femGeometry;
+    this.weight = weight;
+};
+
+FMatrixCalculator.prototype.weightForClickAtNode = function(clickLocation, node) {
+    node = this.geometry.threeGeometry.vertices[node];
+    return this.weight / (node.distanceToSquared(clickLocation) + 1);
+};
+
+FMatrixCalculator.prototype.buildMatrix = function(clickLocation) {
+    var N = this.geometry.threeGeometry.vertices.length;
+
+    var F = [];
+    for(var i = 0; i < N; i++) {
+        F.push(this.weightForClickAtNode(clickLocation, i));
+    }
+
+    return F;
+};
+
+module.exports = FMatrixCalculator;
---
+++ tests/Fem/FMatrixCalculator.js
@@ -0,0 +1,45 @@
+var expect = require('chai').expect;
+var THREE = require('three');
+var FemGeometry = require('../../js/Fem/FemGeometry.js');
+var FMatrixCalculator = require('../../js/Fem/FMatrixCalculator.js');
+
+describe('FMatrixCalculator', function() {
+    it('should exist', function() {
+        expect(FMatrixCalculator).to.be.instanceOf(Function);
+    });
+
+    var calculator;
+
+    beforeEach(function() {
+        var threeGeometry = new THREE.Geometry();
+        threeGeometry.vertices.push(
+            new THREE.Vector3(0,0,0),
+            new THREE.Vector3(1,0,0),
+            new THREE.Vector3(0,1,0)
+        );
+        var femGeometry = new FemGeometry(threeGeometry, []);
+        calculator = new FMatrixCalculator(femGeometry, 1);
+    });
+
+    describe('weightForNode', function() {
+        it('should calculate the weight inversely proportional to distance squared', function() {
+            var weight = calculator.weightForClickAtNode(new THREE.Vector3(1, 1, 0), 0);
+            expect(weight).to.be.closeTo(0.333, 0.001);
+        });
+        it('should not blow up if click is directly at point', function() {
+            var weight = calculator.weightForClickAtNode(new THREE.Vector3(0,0,0),0);
+            expect(weight).to.be.lessThan(Infinity);
+        });
+    });
+
+    describe('buildMatrix', function() {
+        it('should return a correct value', function() {
+            var F = calculator.buildMatrix(new THREE.Vector3(0, 0, 0));
+            expect(F.length).to.equal(3);
+
+            expect(F[0]).to.be.closeTo(1, 0.001);
+            expect(F[1]).to.be.closeTo(0.5, 0.001);
+            expect(F[2]).to.be.closeTo(0.5, 0.001);
+        });
+    });
+});
