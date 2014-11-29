663b0df621ff989ff4dc332600542edfad28bb2c
Chip Bell <chip.bell@spinen.com>
2014-11-29 11:27:34 -0500

    Patching zero vectors to work in sparse matrix math

--- js/util/ccsFullVector.js
+++ js/util/ccsFullVector.js
@@ -1,10 +1,14 @@
 var numeric = require('numeric');
 
 var ccsFullVector = function(sparseVector) {
+
+    if(sparseVector[1].length == 0 && sparseVector[2].length == 0) {
+        sparseVector[1] = [0];
+        sparseVector[2] = [0];
+    }
+
     // make vector full again, then un-nest arrays (so its a flat array instead of 1D matrix)
-    return numeric.ccsFull(sparseVector).map(function(element) {
-        return element[0];
-    });
+    return numeric.ccsFull(sparseVector)[0];
 };
 
 module.exports = ccsFullVector;
--- js/util/ccsSparseVector.js
+++ js/util/ccsSparseVector.js
@@ -2,9 +2,7 @@ var numeric = require('numeric');
 
 var ccsSparseVector = function(vector) {
     // first, convert to a matrix (still a column vector though)
-    return numeric.ccsSparse(vector.map(function(element) {
-        return [element];
-    }));
+    return numeric.ccsSparse([ vector ]);
 };
 
 module.exports = ccsSparseVector;
