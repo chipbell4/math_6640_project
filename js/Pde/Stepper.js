5381285e2377b6908f308e0ccef07347f967e7d3
Chip Bell <chip.bell@spinen.com>
2014-11-28 23:08:54 -0500

    Saving WIP

--- js/Pde/Stepper.js
+++ js/Pde/Stepper.js
@@ -1,26 +1,27 @@
-var numeric = require('numeric');
+var N = require('numeric');
+N.scale = require('../util/scale.js');
+N.ccsScale = require('../util/ccsScale.js');
 var FMatrixCalculator = require('../Fem/FMatrixCalculator.js');
 var StiffnessMatrixCalculator = require('../Fem/StiffnessMatrixCalculator.js');
 var MassMatrixCalculator = require('../Fem/MassMatrixCalculator.js');
 
 var Stepper = function(femGeometry, dampingCoefficient, waveSpeed) {
     this.geometry = femGeometry;
-    this.massMatrix = new MassMatrixCalculator(femGeometry).buildMatrix();
-    this.stiffnessMatrix = new StiffnessMatrixCalculator(femGeometry).buildMatrix();
+    this.massMatrix = N.ccsSparse(new MassMatrixCalculator(femGeometry).buildMatrix());
+    this.stiffnessMatrix = N.ccsSparse(new StiffnessMatrixCalculator(femGeometry).buildMatrix());
     this.dampingCoefficient = this.dampingCoefficient;
     this.waveSpeed = waveSpeed;
     this.currentWavePosition = this.zeroVector();
     this.previousWavePosition = this.zeroVector();
 
     // precalculate some values needed in the calculation
-    //this.inverseMassWithScaling = 
 };
 
 Stepper.prototype.zeroVector = function() {
-    var N = this.massMatrix.length;
+    var n = this.massMatrix.length;
 
     var array = [];
-    for(var i = 0; i < N; i++) {
+    for(var i = 0; i < n; i++) {
         array.push(0);
     }
 
@@ -37,7 +38,12 @@ Stepper.prototype.resolveF = function(mouseClickLocation) {
 Stepper.prototype.step = function(deltaT, mouseClickLocation) {
     var F  = this.resolveF(mouseClickLocation);
 
-    
+    // form the currentWavePosition scale factor
+    var currentScale = N.ccsAdd(
+        N.ccsScale(this.massMatrix, 2 / deltaT / deltaT),
+        N.ccsScale(this.stiffnessMatrix, this.waveSpeed * this.waveSpeed)
+    );
+    var previousScale = N.ccsScale(this.massMatrix, (2 - this.dampingCoefficient * deltaT) / (2 * deltaT));
 };
 
 module.exports = Stepper;
