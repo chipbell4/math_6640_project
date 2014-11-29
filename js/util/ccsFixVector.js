d50fe90426dcebda6bf5f01db1755a5e003fe43a
Chip Bell <chip.bell@spinen.com>
2014-11-29 12:33:43 -0500

    Cleaning up a lint issue

--- js/util/ccsFixVector.js
+++ js/util/ccsFixVector.js
@@ -1,5 +1,5 @@
 var ccsFixVector = function(vector, expectedLength) {
-    if(vector[1].length == 0) {
+    if(vector[1].length === 0) {
         vector[1] = [expectedLength - 1];
     }
     return vector;
