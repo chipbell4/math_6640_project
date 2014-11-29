61439de246c385b8ee42059932ac72be4fda77f0
Chip Bell <chip.bell@spinen.com>
2014-11-28 23:36:55 -0500

    Fixing a lint issue

--- js/util/scale.js
+++ js/util/scale.js
@@ -6,7 +6,7 @@ var scale = function(vector, scaleFactor) {
     var scaled = [];
     for(var i = 0; i < N; i++) {
         if(vector[i] instanceof Array) {
-            scaled.push(scale(vector[i], scaleFactor))
+            scaled.push(scale(vector[i], scaleFactor));
         }
         else {
             scaled.push(scaleFactor * vector[i]);
