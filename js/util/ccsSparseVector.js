8439edd9022873b43da062ede78a838added7a16
Chip Bell <chip.bell@spinen.com>
2014-11-29 11:47:06 -0500

    MOre patches for zero vectors... Hopefully?

--- js/util/ccsSparseVector.js
+++ js/util/ccsSparseVector.js
@@ -1,18 +1,20 @@
 var numeric = require('numeric');
 
+var isZeroVector = function(vector) {
+    return vector.every(function(element) {
+        return element === 0;
+    });
+};
+
 var ccsSparseVector = function(vector) {
-    // first, convert to a matrix (still a column vector though)
-    var sparse = numeric.ccsSparse([ vector ]);
-
-    if(sparse[1].length === 0) {
-        sparse[1] = [0];
+    if(isZeroVector(vector)) {
+        // this is sort of a workaround to numeric's zero-vector bug (that'll probably never be patched)
+        return [ [0, 1], [vector.length - 1], [0] ];
     }
 
-    if(sparse[2].length === 0) {
-        sparse[2] = [0];
-    }
-
-    return sparse;
+    return numeric.ccsSparse(vector.map(function(element) {
+        return [element];
+    })); 
 };
 
 module.exports = ccsSparseVector;
--- tests/util/ccsFullVector.js
+++ tests/util/ccsFullVector.js
@@ -4,13 +4,7 @@ var ccsFullVector = require('../../js/util/ccsFullVector.js');
 
 describe('ccsFullVector', function() {
     it('should work', function() {
-        var sparseColumn = numeric.ccsSparse([ [1, 2, 3] ]);
+        var sparseColumn = numeric.ccsSparse([ [1], [2], [3] ]);
         expect(ccsFullVector(sparseColumn)).to.deep.equal([1, 2, 3]);
-    });
-    
-    it('should work with the zero vector', function() {
-        var sparseColumn = numeric.ccsSparse([ [0, 0, 0] ]);
-        console.log(sparseColumn);
-        expect(ccsFullVector(sparseColumn)).to.deep.equal([0, 0, 0]);
     });
 });
