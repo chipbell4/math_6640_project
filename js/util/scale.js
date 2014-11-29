8db8dd035e19d21cbb195227d0895c4ddce894f5
Chip Bell <chip.bell@spinen.com>
2014-11-28 20:19:39 -0500

    Adding a scale for polyfilling

---
+++ js/util/scale.js
@@ -0,0 +1,19 @@
+/**
+ * A "polyfill" for vector/matrix scaling (which apparently is NOT present in numeric.js :(
+ */
+var scale = function(vector, scaleFactor) {
+    var N = vector.length;
+    var scaled = [];
+    for(var i = 0; i < N; i++) {
+        if(vector[i] instanceof Array) {
+            scaled.push(scale(vector[i], scaleFactor))
+        }
+        else {
+            scaled.push(scaleFactor * vector[i]);
+        }
+    } 
+
+    return scaled;
+};
+
+module.exports = scale;
